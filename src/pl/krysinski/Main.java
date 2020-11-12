2891e056204ffcf87327838852ed014f911fd802
Marcin <marcin.jakubkrysinski@gmail.com>
2020-11-12 22:42:02 +0100

    Added Animal, Human and Phone class - taks 1 finished.

--- src/pl/krysinski/Main.java
+++ src/pl/krysinski/Main.java
@@ -1,8 +1,23 @@
 package pl.krysinski;
 
+import pl.krysinski.module_1.model.Animal;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Animal dog = new Animal("dog");
+        Animal cat = new Animal("cat");
+        System.out.println(dog);
+        System.out.println(cat);
+
+        for (int i = 0; i < 3; i++) {
+            cat.feed();
+        }
+        for (int i = 0; i < 11; i++) {
+            cat.takeAWalk();
+        }
+        cat.feed();
     }
+
 }
