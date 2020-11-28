61d9d39e9aeb2086141db60a0676a618f0194b34
Marcin <marcin.jakubkrysinski@gmail.com>
2020-11-28 11:44:10 +0100

    poprawki

--- src/pl/krysinski/module_1/model/devices/Phone.java
+++ src/pl/krysinski/module_1/model/devices/Phone.java
@@ -4,11 +4,13 @@ public class Phone extends Device{
 
     private String producer;
     private String model;
-    Integer yearOfProduction;
+    private Integer yearOfProduction;
     private Double screenSize;
     private String androidOrIos;
 
-    public Phone(String producer, String model, Integer yearOfProduction) {
+    public Phone(String producer, String model, Integer yearOfProduction, Double screenSize, String androidOrIos) {
         super(producer, model, yearOfProduction);
+        this.screenSize = screenSize;
+        this.androidOrIos =androidOrIos;
     }
 }
